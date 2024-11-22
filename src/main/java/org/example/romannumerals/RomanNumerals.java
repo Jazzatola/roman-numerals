82cc73263c4e5bab35c76f3cf281d3c05e877452
James Maggs <github@jamesmaggs.com>
2024-11-22 17:12:48 +0000

    Make the map a bit cleaner but the sorting is still ugly.

--- src/main/java/org/example/romannumerals/RomanNumerals.java
+++ src/main/java/org/example/romannumerals/RomanNumerals.java
@@ -1,33 +1,30 @@
 package org.example.romannumerals;
 
-import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class RomanNumerals {
 
-    private static final Map<Integer, String> romans = new TreeMap<>(RomanNumerals::reverseOrder);
-
-    private static int reverseOrder(int first, int second) {
-        return second - first;
-    }
-
-    static {
-        romans.put(1, "I");
-        romans.put(5, "V");
-        romans.put(10, "X");
-    }
+    private static final Map<Integer, String> romanNumerals = Map.of(
+        1, "I",
+        5, "V",
+        10, "X"
+    );
 
     public static String fromDecimal(int decimal) {
         StringBuilder output = new StringBuilder();
 
-        for(Integer key: romans.keySet()) {
+        for(Integer key: decimalKeysInReverseOrder()) {
             while(decimal >= key) {
-                output.append(romans.get(key));
+                output.append(romanNumerals.get(key));
                 decimal -= key;
             }
         }
 
         return output.toString();
+    }
+
+    private static List<Integer> decimalKeysInReverseOrder() {
+        return romanNumerals.keySet().stream().sorted((first, second) -> second - first).toList();
     }
 }
