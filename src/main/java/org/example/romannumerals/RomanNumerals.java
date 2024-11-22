b394583c703e2b84e01b69913bb7efcd3e6e7cdf
James Maggs <github@jamesmaggs.com>
2024-11-22 16:57:34 +0000

    Move the symbols into a map and loop over that.

--- src/main/java/org/example/romannumerals/RomanNumerals.java
+++ src/main/java/org/example/romannumerals/RomanNumerals.java
@@ -1,22 +1,31 @@
 package org.example.romannumerals;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class RomanNumerals {
+
+    private static final Map<Integer, String> romans = new TreeMap<>(RomanNumerals::reverseOrder);
+
+    private static int reverseOrder(int first, int second) {
+        return second - first;
+    }
+
+    static {
+        romans.put(1, "I");
+        romans.put(5, "V");
+        romans.put(10, "X");
+    }
+
     public static String fromDecimal(int decimal) {
         StringBuilder output = new StringBuilder();
 
-        while(decimal >= 10) {
-            output.append("X");
-            decimal -= 10;
-        }
-
-        while(decimal >= 5) {
-            output.append("V");
-            decimal -= 5;
-        }
-
-        while(decimal > 0) {
-            output.append("I");
-            decimal--;
+        for(Integer key: romans.keySet()) {
+            while(decimal >= key) {
+                output.append(romans.get(key));
+                decimal -= key;
+            }
         }
 
         return output.toString();
