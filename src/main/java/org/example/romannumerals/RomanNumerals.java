85e3cc6b0aaf84668d784f6cc379b92d3bf11c69
James Maggs <github@jamesmaggs.com>
2024-11-23 12:45:58 +0000

    Tidy up.

--- src/main/java/org/example/romannumerals/RomanNumerals.java
+++ src/main/java/org/example/romannumerals/RomanNumerals.java
@@ -3,23 +3,19 @@ package org.example.romannumerals;
 public class RomanNumerals {
 
     private enum RomanNumeral {
-        M(1000),
-        CM(900),
-        D(500),
-        C(100),
-        XC(90),
-        L(50),
-        XL(40),
-        X(10),
-        IX(9),
-        V(5),
-        IV(4),
-        I(1);
+        M(1000), CM(900), D(500),
+        C(100), XC(90), L(50),
+        XL(40), X(10), IX(9),
+        V(5), IV(4), I(1);
 
-        private final Integer decimal;
+        private final int decimal;
 
-        RomanNumeral(Integer decimal) {
+        RomanNumeral(int decimal) {
             this.decimal = decimal;
+        }
+
+        public int decimal() {
+            return decimal;
         }
     }
 
@@ -27,9 +23,9 @@ public class RomanNumerals {
         StringBuilder output = new StringBuilder();
 
         for(RomanNumeral numeral: RomanNumeral.values()) {
-            while(decimal >= numeral.decimal) {
+            while(decimal >= numeral.decimal()) {
                 output.append(numeral.name());
-                decimal -= numeral.decimal;
+                decimal -= numeral.decimal();
             }
         }
 
